c92a1244a5cc35f4c3c70b9b3e51bbe1457ee86b
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 21:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-//your code here
 document.addEventListener('DOMContentLoaded', () => {
-	const images = document.querySelectorAll('.image');
+  const images = document.querySelectorAll('.image');
 
-	let draggedElement = null;
+  let draggedElement = null;
 
-	images.forEach((image) => {
-		image.addEventListener("dragstart", (event)=> {
-			draggedElement = event.target;
-			event.target.classList.add("selected");
-		});
+  images.forEach((image) => {
+    image.addEventListener('dragstart', (event) => {
+      draggedElement = event.target;
+      event.target.classList.add('selected');
+    });
 
-		image.addEventListener("dragover", (event) => {
-			event.preventDefault();
-		});
+    image.addEventListener('dragover', (event) => {
+      event.preventDefault();
+    });
 
-		image.addEventListener("dragend", () => {
-			draggedElement.classList.remove("selected");
-			draggedElement = null;
-		});
+    image.addEventListener('dragend', () => {
+      if (draggedElement) {
+        draggedElement.classList.remove('selected');
+      }
+      draggedElement = null;
+    });
 
-		image.addEventListener("drop", (event) => {
-			event.preventDefault();
-			if(event.target !== draggedElement) {
-				const draggedHTML = draggedElement.innerHTML;
-				draggedElement.innerHTML = event.target.innerHTML;
-				event.target.innerHTML = draggedHTML;
-				const draggedImg = draggedElement.style.backgroundImage;
-				draggedElement.style.backgroundImage = event.target.style.backgroundImage;
-				event.target.style.backgroundImage = draggedImg;
-			}
-		})
-	})
-})
+    image.addEventListener('drop', (event) => {
+      event.preventDefault();
+      if (event.target !== draggedElement) {
+
+        const draggedHTML = draggedElement.innerHTML;
+        draggedElement.innerHTML = event.target.innerHTML;
+        event.target.innerHTML = draggedHTML;
+
+        const draggedBg = draggedElement.style.backgroundImage;
+        draggedElement.style.backgroundImage = event.target.style.backgroundImage;
+        event.target.style.backgroundImage = draggedBg;
+
+        const draggedId = draggedElement.id;
+        draggedElement.id = event.target.id;
+        event.target.id = draggedId;
+      }
+    });
+  });
+});
